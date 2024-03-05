f9016b385303c94b558be948e6dd28684cdfee0a
DucaAleXD <ducasasha@gmail.com>
2024-03-05 09:18:42 +0200

    update la migrari

--- database/migrations/2024_03_05_062005_create_cities_table.php
+++ database/migrations/2024_03_05_062005_create_cities_table.php
@@ -6,26 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->string('nume');
-            $table->integer('populatie');
-            $table->string('country_id');
-            $table->string('zona_geografica');
-            $table->timestamps();
-            $table->foregin('country_id')->on('countries');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+/** * Run the migrations. */ public function up(): void
+{
+    Schema::create('cities', function (Blueprint $table) {
+    $table->id();
+    $table->string('nume');
+    $table->integer('populatie');
+    $table->unsignedBigInteger('country_id');
+    $table->string('zona_geografica');
+    $table->timestamps();
+    $table->foreign('country_id')
+    ->references('id')
+    ->on('countries');
+    });
+}
+public function down(): void
     {
         Schema::dropIfExists('cities');
     }
