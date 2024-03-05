8602df079a5d18d761dcd2003dac16101927ff75
DucaAleXD <ducasasha@gmail.com>
2024-03-05 08:37:30 +0200

    facem relatia

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class City extends Model
 {
@@ -15,4 +16,7 @@ class City extends Model
         'country_id',
         'zona_geografica'
     ];
+    public function country():BelongsTo{
+
+    }
 }
